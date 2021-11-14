81077e6969e4d96594ce2acb45c8d5c723e227dd
Doni-zete <donizete112@gmail.com>
2021-11-14 00:13:12 -0300

    visualização grafica

--- Exercicios/prog04.java
+++ Exercicios/prog04.java
@@ -4,14 +4,10 @@ import javax.swing.*;
 public class prog04 {
   public static void main(String[] args) {
     int dia = 0, mes = 0, ano = 0;
-    String aux1 = JOptionPane.showInputDialog(null, "Informe o dia (ex:31): ");
-    String aux2 = JOptionPane.showInputDialog(null, "Informe o mes (ex:12): ");
-    String aux3 = JOptionPane.showInputDialog(null, "Informe o ano (ex:2017): ");
-    
 
-    dia = Integer.parseInt(aux1);
-    mes = Integer.parseInt(aux2);
-    ano = Integer.parseInt(aux3);
+    dia = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o dia (ex:31): "));
+    mes = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o mes (ex:12): "));
+    ano = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o ano (ex:2017): "));
 
     JOptionPane.showMessageDialog(null, dia + "/" + mes + "/" + ano);
   }
