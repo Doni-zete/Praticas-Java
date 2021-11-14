8deaf483291ff7d74cdefbb84ccd92a291797b99
Doni-zete <donizete112@gmail.com>
2021-11-13 23:44:26 -0300

    prog

---
+++ Exercicios/prog03.java
@@ -0,0 +1,25 @@
+package Exercicios;
+
+// Teste de entrada de dados
+import java.util.Scanner;
+
+public class prog03 {
+  public static void main(String[] args) {
+
+    float nota1, nota2, media;
+    Scanner entrada = new Scanner(System.in);
+
+    System.out.print("Digite a nota 1:");
+    nota1 = Float.parseFloat(entrada.nextLine());
+
+    System.out.print("Digite a nota 2:");
+    nota2 = Float.parseFloat(entrada.nextLine());
+
+    media = (nota1 + nota2) / 2;
+
+    System.out.println("Media:" + media);
+    System.out.println("Fim do programa");
+
+  }
+
+}
