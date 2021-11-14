238070acd8d46c583b2f465e803c9cc4749b0308
Doni-zete <donizete112@gmail.com>
2021-11-13 23:23:28 -0300

    exercicios

---
+++ Exercicios/Hello.java
@@ -0,0 +1,6 @@
+package Exercicios;
+ class Hello {
+  public static void main(String[] args) throws Exception {
+      System.out.println("Hello, World!");
+  }
+}
---
+++ Exercicios/prog02.java
@@ -0,0 +1,21 @@
+package Exercicios;
+public class prog02 {
+  public static void main(String[] args) {
+
+    // delcarando valores
+    int valor1 = 10, valor2 = 3;
+    String valor3 = "100";
+
+    // exibindo valores
+    System.out.println("Valor de v1:" + valor1);
+    System.out.println("Valor de v2:" + valor2);
+
+    // realizando operaçoes
+    System.out.println("\nResultado adicao: " + (valor1 + valor2));
+    System.out.println("Resultado multiplicaco: " + (valor1 * valor2));
+    System.out.println("Resultado divisão: " + (valor1%valor2));
+    System.out.println("Resultado adicao: " +(valor1+valor2+valor3));
+    System.out.println("\nFim do programa.");
+  }
+
+}
