3690a44d83cdb58eb9c623a98f3c77b4e306431c
Doni-zete <donizete112@gmail.com>
2021-11-17 10:14:58 -0300

    if else

---
+++ Exercicios/prog005.java
@@ -0,0 +1,12 @@
+
+public class prog005 {
+
+  public static void main(String[] args) {
+    int idade = 10;
+    if (idade >= 18) {
+      System.out.println("Voce ja e de maior ");
+    } else {
+      System.out.println("Voce e de menor");
+    }
+  }
+}
