6d2ebd947535e569e1d9e31f286e1b66e6eb0c28
Doni-zete <donizete112@gmail.com>
2021-11-17 21:01:48 -0300

    programs

---
+++ Exercicios/Proghome7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Proghome7 {
+
+  public static void main(String[] args) {
+
+    Scanner entrada =new Scanner(System.in);
+
+    float nota1 = 0, nota2 = 0, media;
+
+    media = (nota1 + nota2) / 2;
+
+    System.out.print(media);
+
+  }
+
+}
---
+++ Exercicios/ternario.java
@@ -0,0 +1,9 @@
+public class ternario {
+  public static void main(String[] args) {
+
+    int idade = 32;
+    String b = (idade >= 18) ? "Voce ja e de maior" : "Voce ja e de menor";
+    System.out.println("Resposta: " + b);
+
+  }
+}
