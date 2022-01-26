fac99e3496c90de4b12ae41459a753774a406d63
Donizete Crisostomo <donizete112@gmail.com>
2022-01-25 23:54:51 -0300

    Ex7_OrdemInversa

--- ExerciciosDio/Ex7_OrdemInversa.java
+++ ExerciciosDio/Ex7_OrdemInversa.java
@@ -4,13 +4,23 @@ e mostre-os na ordem inversa.
 */
 
 public class Ex7_OrdemInversa {
-  
+
   public static void main(String[] args) {
-    
-    int [] vetor ={5,-16,60,80,2,9};
 
-    System.out.println(vetor);
+    int[] vetor = { 5, -16, 60, 80, 2, 9 };
 
+    System.out.print("\nVetor: ");
+    int contador = 0;
+    while (contador < (vetor.length)) {
+
+      System.out.print(vetor[contador] + " ");
+      contador++;
+    }
+
+    System.out.print("\nVetor: ");
+    for (int i = (vetor.length - 1); i >= 0; i--) {
+      System.out.print(vetor[i] + " ");
+    }
 
   }
 }
